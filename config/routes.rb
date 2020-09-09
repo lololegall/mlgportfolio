ec6d031473a0ae9c58afdd898db6c93dbceaae6b
Margaux Le Gall <margauxxlegall@gmail.com>
2020-09-09 21:13:00 +0200

    postgres

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'work', to: 'pages#work'
+  get 'whatido', to: 'pages#whatido'
+  get 'margalou', to: 'pages#margalou'
+  get 'sucre-sale', to: 'pages#sucresale'
+  get 'defi-wind', to: 'pages#defiwind'
+  get 'un-chien-en-ville', to: 'pages#unchienenville'
 end
